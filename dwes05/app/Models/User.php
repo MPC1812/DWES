034803e255555b22163fb46de19bfa33065ce592
Blackmoon1812 <93385348+Blackmoon1812@users.noreply.github.com>
2025-03-08 18:56:34 +0100

    Modificado el modelo User.php
    Se añade la relación entre tablas y un archivo txt con los pasos que voy dando.

--- dwes05/app/Models/User.php
+++ dwes05/app/Models/User.php
@@ -42,4 +42,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function mascotas()
+    {
+        return $this->hasMany(MascotaMPC::class);
+    }
 }
